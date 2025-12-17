706cdf46a8dcbe483c217fd5d2736100a6745c6b
Mark <jaimepersonal46@gmail.com>
2025-12-17 19:34:49 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,5 +1,6 @@
 -- Please Any Issues Report To Me With SS of the error --
 
-loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049", true))()
-task.wait(2)
 loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/loader.lua', true))()
+task.wait(2)
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049", true))()
+
