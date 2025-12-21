bdbc7b4e01a53f8a1083a40881c5075be8c278be
Mark <jaimepersonal46@gmail.com>
2025-12-21 11:23:17 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,7 +1,7 @@
 -- Please Any Issues Report To Me With SS of the error --
 local fenv = getfenv()
 task.spawn(function()
-    loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049"))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/frFvztQ8JR/0"))()
 end)
 loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216703"))()
 
